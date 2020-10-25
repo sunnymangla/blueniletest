3c731d0aabd1bacf81b5ea8d5b156759ed6a2a34
Sunny <smangla@lululemon.com>
2020-10-24 19:09:59 -0700

    update changes

--- src/main/java/com/store/BluenileRequestHandler.java
+++ src/main/java/com/store/BluenileRequestHandler.java
@@ -1,11 +1,19 @@
 package com.store;
 
 import com.google.api.client.http.HttpMethods;
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.rpc.ApiException;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.ServiceOptions;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 import com.google.cloud.pubsub.v1.Publisher;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.gson.*;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.ProjectTopicName;
@@ -14,12 +22,14 @@ import org.apache.http.HttpStatus;
 
 
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -152,20 +162,76 @@ public class BluenileRequestHandler implements HttpFunction {
 
                 publisher = Publisher.newBuilder(topicName).build();
 
+                //Credentials creds = ServiceAccountCredentials.fromStream(new FileInputStream(System.getenv("GOOGLE_APPLICATION_CREDENTIALS")));
+
+               /* publisher = Publisher.newBuilder(
+                        topicName)
+                        .setCredentialsProvider(FixedCredentialsProvider.create(creds))
+                        .build();*/
+
+                publisher = Publisher.newBuilder(
+                        topicName)
+                        .build();
+
                 PubsubMessage pubsubMessage =
                         PubsubMessage.newBuilder().setData(ByteString.copyFromUtf8(payloadMessage)).build();
                 logger.log(Level.INFO, "pub sub message is -" + pubsubMessage.toString());
 
                 //publish payload to pub/sub
-                publisher.publish(pubsubMessage);
-                publisher.publishAllOutstanding();
+                ApiFuture<String> future = publisher.publish(pubsubMessage);
 
-            }
 
-        } catch (IOException io) {
+
+                // Add an asynchronous callback to handle success / failure
+                ApiFutures.addCallback(
+                        future,
+                        new ApiFutureCallback<String>() {
+
+                            @Override
+                            public void onFailure(Throwable throwable) {
+                                throwable.printStackTrace();
+                                if (throwable instanceof ApiException) {
+                                    ApiException apiException = ((ApiException) throwable);
+                                    // details on the API exception
+                                    System.out.println(apiException.getStatusCode().getCode());
+                                    System.out.println(apiException.isRetryable());
+                                }
+                                System.out.println("Error publishing message : " + pubsubMessage);
+                            }
+
+                            @Override
+                            public void onSuccess(String messageId) {
+                                // Once published, returns server-assigned message ids (unique within the topic)
+                                System.out.println(messageId);
+                            }
+                        },
+                        MoreExecutors.directExecutor());
+
+
+
+            }}
+
+        catch (IOException io) {
             logger.log(Level.SEVERE, "IO Exception");
             io.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
         }
+
+        finally {
+                if (publisher != null) {
+                    // When finished with the publisher, shutdown to free up resources.
+                    System.out.println("shutdown");
+                    publisher.shutdown();
+                   try {
+                       publisher.awaitTermination(1, TimeUnit.MINUTES);
+                   }catch(InterruptedException ire){
+                       ire.printStackTrace();
+                   }
+                }
+            }
+
+
     }
 
     private Publisher publisher;
